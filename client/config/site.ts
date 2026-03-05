8f5d5853131cd8ab0910266a366227cc9e07d868
Zhusha7 <andzej200@gmail.com>
2026-03-05 21:04:31 +0200

    Add base routes, test theme

--- client/config/site.ts
+++ client/config/site.ts
@@ -1,69 +1,51 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Психотерапевт | Е. Ч.",
+  description: "Клинический нейропсихолог, Екатерина Чулкова.",
   navItems: [
+    {
+      label: "Домашняя страница",
+      href: "/",
+    },
+    {
+      label: "Обо мне",
+      href: "/about",
+    },
+    {
+      label: "Специализации",
+      href: "/specialties",
+    },
+    {
+      label: "Контакты",
+      href: "/contact",
+    },
+    {
+      label: "Условия терапии",
+      href: "/terms",
+    },
+  ],
+  navMenuItems: [
     {
       label: "Home",
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
       label: "About",
       href: "/about",
     },
-  ],
-  navMenuItems: [
     {
-      label: "Profile",
-      href: "/profile",
+      label: "Specialties",
+      href: "/specialties",
     },
     {
-      label: "Dashboard",
-      href: "/dashboard",
+      label: "Contact",
+      href: "/contact",
     },
     {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
+      label: "Terms of Service",
+      href: "/terms",
     },
   ],
-  links: {
-    github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
+  links: {},
 };
